ae125acd2fc80f2d050c15a16face9b1436f8e76
karot.chuang <karot.chuang@coretronic.com>
2016-12-19 10:42:08 +0800

    no message

--- app/src/main/java/chuang/karote/actionslistgenerator/ui/CalenderLogFragment.java
+++ app/src/main/java/chuang/karote/actionslistgenerator/ui/CalenderLogFragment.java
@@ -1,5 +1,10 @@
 package chuang.karote.actionslistgenerator.ui;
 
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.view.Window;
+
 import com.roomorama.caldroid.CaldroidFragment;
 import com.roomorama.caldroid.CaldroidGridAdapter;
 
@@ -14,5 +19,15 @@ public class CalenderLogFragment extends CaldroidFragment {
     @Override
     public CaldroidGridAdapter getNewDatesGridAdapter(int month, int year) {
         return new CalenderLogAdapter(getActivity(), month, year, getCaldroidData(), extraData);
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Dialog dialog = super.onCreateDialog(savedInstanceState);
+
+        // request a window without the title
+        dialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        return dialog;
     }
 }
